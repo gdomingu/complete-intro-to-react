9c4977b77eca384be75295927155c88cfb9b59e8
Gabe D <gpad88@gmail.com>
2016-03-24 08:59:13 -0700

    bundle.js

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,26 +1,10 @@
+// Webpack is a build tool. Better to have local scopes, without polluting the global namespace.
+// His favorite is rollup.js
+var React = require('react')
+// react corresponds to what npm installed
+var ReactDOM = require('react-dom')
+var MyTitle = require('./MyTitle')
 var div = React.DOM.div
-var h1 = React.DOM.h1
-// React is how you write the components
-// the null is the html attributes
-// like { style: { color: 'red'}}
-
-// this is a composit component
-// Reusable that you can use all over the place
-// Like a comment component that you can use anywhere
-// Can also nest component
-
-var MyTitle = React.createClass({
-  // must render
-  render () {
-    // must return a component
-    return (
-      div(null,
-        // has access to the properties that you passed in create element
-        h1({style: {color: this.props.color}}, this.props.title)
-      )
-    )
-  }
-})
 
 var MyTitleFactory = React.createFactory(MyTitle)
 // create factory is not used with JSX
@@ -28,11 +12,10 @@ var ce = React.createElement
 // Create instance of class
 var MyFirstComponent = (
   div(null,
-    MyTitleFactory({ title: 'Props are great!', color: 'rebeccapurple'}),
+    MyTitleFactory({title: 'Props are great!', color: 'rebeccapurple'}),
     React.createElement(MyTitle, {title: 'Use props everywhere', color: 'mediumaquamarine'}),
     ce(MyTitle, {title: 'props are the best!', color: 'papayawhip'})
     )
   )
-// 
 ReactDOM.render(MyFirstComponent, document.getElementById('app'))
 // ReactDOM is how you render it.
