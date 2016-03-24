ea997b4462b987d51a8e58726cc852596c2bbcf2
Gabe D <gpad88@gmail.com>
2016-03-24 10:01:27 -0700

    lunch

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -23,5 +23,15 @@ var MyFirstComponent = function (){
 // must use / in closing tag in jsx
 // If it is Capitalized like MyTitle it is something I created.
 // A composit component.
+// can use 
+
+// var MyFirstComponent = () => (
+//     <div>
+//       <MyTitle title='Whatever' color='rebeccapurple' />
+//       <input />
+//       <MyTitle title='LOL' color='dodgersblue' />
+//       <MyTitle title='MOMOMOMOOO' color='#f06d06' />
+//     </div>
+//  )
 ReactDOM.render(<MyFirstComponent />, document.getElementById('app'))
 // ReactDOM is how you render it.
