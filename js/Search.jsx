b6b896f6534789e87cc0d18d7e83c75f47c64b93
Gabe D <gpad88@gmail.com>
2016-03-25 12:22:52 -0700

    search working again with redux

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,32 +3,30 @@ const ShowCard = require('./ShowCard')
 // const data = require('../public/data')
 // now passing it in as a prop from client app
 const Header = require('./Header')
-const { object } = React.PropTypes
+const { object, string } = React.PropTypes
+const { connector } = require('./Store')
 
 const Search = React.createClass({
-  getInitialState () {
-    return {
-      searchTerm: ''
-    }
-  },
+  // getInitialState () {
+  //   return {
+  //     searchTerm: ''
+  //   }
+  // },
   propTypes: {
-    route: object
+    route: object,
+    searchTerm: string
   },
   // This is highly encouraged for future self
   // this shows up when first rendered
-  handleSearchTermChange (searchTerm) {
-    this.setState({ searchTerm })
-  },
+  // handleSearchTermChange (searchTerm) {
+  //   this.setState({ searchTerm })
+  // },
   // this will be called onChange
   // This is where you set this.state online 17.
   render () {
     return (
       <div className='container'>
-        <Header
-          handleSearchTermChange={this.handleSearchTermChange}
-          searchTerm={this.state.searchTerm}
-          showSearch
-        />
+        <Header showSearch />
         <div className='shows'>
           {this.props.route.shows
             .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
@@ -46,7 +44,7 @@ const Search = React.createClass({
 // can do <ShowCard {...show} />
 // <ShowCard show={show} key={show.imdbID} />
 
-module.exports = Search
+module.exports = connector(Search)
 // shows is data you want to show
 // null is
 // 4 is number of spaces
