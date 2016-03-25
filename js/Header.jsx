f9fbdfaaf92b391bbecf09debe2734d95d6102d5
Gabe D <gpad88@gmail.com>
2016-03-25 11:20:58 -0700

    working on header

---
+++ js/Header.jsx
@@ -0,0 +1,37 @@
+const React = require('react')
+const { Link } = require('react-router')
+const { func, bool, string } = React.PropTypes
+
+const Header = React.createClass({
+  propTypes: {
+    handleSearchTermChange: func,
+    showSearch: bool,
+    searchTerm: string
+  },
+  handleSearchTermEvent (event) {
+    this.props.handleSearchTermChange(event.target.value)
+  },
+  render () {
+    let utilSpace
+    // component we are going to use
+    if (this.props.showSearch) {
+      utilSpace = <input value={this.state.searchTerm} className='search-input' type='text' placeholder='Search' onChange={this.handleSearchTermEvent} />
+    } else {
+      utilSpace = (
+        <h2 className='header-back'>
+          <Link to='/search'>Back</Link>
+        </h2>
+      )
+    }
+    return (
+      <header className='header'>
+        <h1 className='brand'>
+          <Link to='/' className='brand-link'>svideo</Link>
+        </h1>
+        {utilSpace}
+      </header>
+    )
+  }
+})
+// {utilSpace} is a variable with already rendered jsx so you can just use it like that.
+module.exports = Header
--- js/Search.jsx
+++ js/Search.jsx
@@ -2,6 +2,7 @@ const React = require('react')
 const ShowCard = require('./ShowCard')
 // const data = require('../public/data')
 // now passing it in as a prop from client app
+const Header = require('./Header')
 const { object } = React.PropTypes
 
 const Search = React.createClass({
@@ -15,18 +16,19 @@ const Search = React.createClass({
   },
   // This is highly encouraged for future self
   // this shows up when first rendered
-  handleSearchTermEvent (event) {
-    this.setState({ searchTerm: event.target.value })
+  handleSearchTermChange (searchTerm) {
+    this.setState({ searchTerm: searchTerm })
   },
   // this will be called onChange
   // This is where you set this.state online 17.
   render () {
     return (
       <div className='container'>
-        <header className='header'>
-          <h1 className='brand'>svideo</h1>
-          <input value={this.state.searchTerm} className='search-input' type='text' placeholder='Search' onChange={this.handleSearchTermEvent} />
-        </header>
+        <Header
+          handleSearchTermChange={this.handleSearchTermChange}
+          searchTerm={this.state.searchTerm}
+          showSearch
+        />
         <div className='shows'>
           {this.props.route.shows
             .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
